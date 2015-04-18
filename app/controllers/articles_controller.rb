ab26e8868cc1108ab6713e5f36f027045d869eca
Cornellius Ngondo <devngondo@gmail.com>
2015-04-18 14:23:43 +0000

    Added create action and modified articles_helper.rb to create module to be used in articles_controller.rb

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,5 @@
 class ArticlesController < ApplicationController
+  include ArticlesHelper
   def index
     @articles = Article.all
   end
@@ -11,4 +12,10 @@ class ArticlesController < ApplicationController
     @article = Article.new
   end
 
+  def create
+    @article = Article.new(article_params)
+    @article.save
+
+    redirect_to article_path(@article)
+  end
 end
