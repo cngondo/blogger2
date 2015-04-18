bd8abdfab737609c5f7c28091475292a38b8e51f
Cornellius Ngondo <devngondo@gmail.com>
2015-04-18 16:20:46 +0000

    Added flash message for actions

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -15,7 +15,8 @@ class ArticlesController < ApplicationController
   def create
     @article = Article.new(article_params)
     @article.save
-
+    # Flash notice to show creation of article
+    flash.notice = "Article '#{@article.title}' Created!"
     redirect_to article_path(@article)
   end
 
@@ -26,6 +27,8 @@ class ArticlesController < ApplicationController
   def update
     @article = Article.find(params[:id])
     @article.update(article_params)
+    # Flash notice to show update of article
+    flash.notice = "Article '#{@article.title}' Updated!"
 
     redirect_to article_path(@article)
   end
@@ -33,6 +36,8 @@ class ArticlesController < ApplicationController
   def destroy
     @article = Article.find(params[:id])
     @article.destroy
+    # Flash notice to show deletion of article
+    flash.notice = "Article '#{@article.title}' Deleted!"
     redirect_to action: 'index'
   end
 end
