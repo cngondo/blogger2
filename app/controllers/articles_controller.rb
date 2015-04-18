909605afe6aadaf442963f52b12240db1bd70f22
Cornellius Ngondo <devngondo@gmail.com>
2015-04-18 18:14:56 +0000

    Added comments and hooked it up with the UI

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -6,6 +6,9 @@ class ArticlesController < ApplicationController
 
   def show
     @article = Article.find(params[:id])
+    # to show the comments
+    @comment = Comment.new
+    @comment.article_id = @article.id
   end
 
   def new
