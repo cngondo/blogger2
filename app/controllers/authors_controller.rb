020a5e1472dea83d933c25acd9ea43702786f596
Cornellius Ngondo <devngondo@gmail.com>
2015-04-21 01:58:49 +0000

    Implemented Basic authentication

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -1,4 +1,14 @@
 class AuthorsController < ApplicationController
+  # Protection scheme for the new users form
+  before_filter :zero_authors_or_authenticated, only: [:new, :create]
+
+  def zero_authors_or_authenticated
+    unless Author.count == 0 || current_user
+      redirect_to root_path
+      return false
+    end
+  end
+
   before_action :set_author, only: [:show, :edit, :update, :destroy]
 
   # GET /authors
